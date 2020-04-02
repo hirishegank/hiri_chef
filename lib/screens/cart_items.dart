a43424177fb1c8737aaf456cd93dcfb1b9ef6e9d
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-04-03 01:18:22 +0530

    modified menu cards

--- lib/screens/cart_items.dart
+++ lib/screens/cart_items.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/fa_icon.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:user/components/big_button.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+// import 'package:font_awesome_flutter/fa_icon.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:user/components/big_button.dart';
 
-import 'package:user/components/delete_icon_button.dart';
-import 'package:user/components/home_category_buttom.dart';
-import 'package:user/screens/customize_screen.dart';
-import 'package:user/screens/delivery_option.dart';
+// import 'package:user/components/delete_icon_button.dart';
+// import 'package:user/components/home_category_buttom.dart';
+// import 'package:user/screens/customize_screen.dart';
+// import 'package:user/screens/delivery_option.dart';
 
 class CartPage extends StatefulWidget {
   @override
@@ -24,21 +25,30 @@ class _CartPageState extends State<CartPage> {
         centerTitle: true,
       )),
       floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        // Add your onPressed code here!
-      },
-      child: Icon(Icons.navigation),
-      backgroundColor: Colors.green,
-    ),
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Icon(Icons.navigation),
+        backgroundColor: Colors.green,
+      ),
       body: SafeArea(
         child: ListView(
           children: <Widget>[
-            CartItemCard(),
-            CartItemCard(),
-            CartItemCard(),
-            CartItemCard(),
-            CartItemCard(),
-            CartItemCard(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                children: <Widget>[
+                  MenuCard(),
+                  MenuCard(),
+                  MenuCard(),
+                  MenuCard(),
+                  MenuCard(),
+                  MenuCard(),
+                  MenuCard(),
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -46,112 +56,67 @@ class _CartPageState extends State<CartPage> {
   }
 }
 
-class CartItemCard extends StatefulWidget {
-  const CartItemCard({
+class MenuCard extends StatelessWidget {
+  const MenuCard({
     Key key,
   }) : super(key: key);
 
   @override
-  _CartItemCardState createState() => _CartItemCardState();
-}
-
-class _CartItemCardState extends State<CartItemCard> {
-  double ammount = 400;
-  int quantity = 1;
-  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15),
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade100,
-        borderRadius: BorderRadius.circular(10),
-      ),
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      width: (MediaQuery.of(context).size.width - 80) / 2,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  child: Image.asset(
-                    'assets/img/cartFoodSample.png',
-                    fit: BoxFit.cover,
-                  ),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              Expanded(
-                flex: 5,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            'Keerai Putt',
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        '4.1 (Rating)',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              child: Row(
-                                children: <Widget>[
-                                  Container(
-                                    margin: EdgeInsets.symmetric(horizontal: 3),
-                                    child: Center(
-                                      
-                                    ),
-                                    width: 50,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        border:
-                                            Border.all(color: Colors.green)),
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                   
-                     
-                    ],
-                  ),
-                ),
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Image.asset(
+              'assets/img/walk1.png',
+              width: (MediaQuery.of(context).size.width - 120) / 2,
+              height: (MediaQuery.of(context).size.width - 120) / 2 - 10,
+              fit: BoxFit.cover,
+            ),
           ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Food Name',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+            child: Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: <Widget>[
+                Text(
+                  '4.1',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+                RatingBarIndicator(
+                  rating: 4.1,
+                  direction: Axis.horizontal,
+                  itemCount: 5,
+                  itemSize: 12,
+                  unratedColor: Colors.green.shade100,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.green,
+                  ),
+                ),
+                Text(
+                  '(200)',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                )
+              ],
+            ),
+          )
         ],
       ),
+      height: 250,
+      color: Colors.grey.shade200,
     );
   }
 }
-
-
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:user/components/best_food_card.dart';
-import 'package:user/components/home_category_buttom.dart';
-import 'package:user/components/popular_food_card.dart';
-import 'package:user/constants.dart';
+// import 'package:user/components/best_food_card.dart';
+// import 'package:user/components/home_category_buttom.dart';
+// import 'package:user/components/popular_food_card.dart';
+// import 'package:user/constants.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -15,133 +15,270 @@ class _HomePageState extends State<HomePage> {
   String searchText;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor:Colors.green,
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Text('Past')),
-                Tab(icon: Text('Ongoing')),
-                Tab(icon: Text('Pending')),
-              ],
-            ),
-            title: Text('Track Orders'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          bottom: TabBar(
+            indicatorColor: Colors.green,
+            indicatorSize: TabBarIndicatorSize.label,
+            tabs: [
+              Tab(
+                  icon: Text(
+                'Past',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Muli',
+                    fontWeight: FontWeight.w300),
+              )),
+              Tab(
+                  icon: Text('Ongoing',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.w300))),
+              Tab(
+                  icon: Text('Pending',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.w300))),
+            ],
           ),
-          body: TabBarView(
+          title: Align(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Hello cook Kabali !',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+        ),
+        body: SafeArea(
+          child: TabBarView(
             children: [
-              Column(
-                children:[CardItems(),CardItems()],),
-              Column(
-                children:[CardItems(),CardItems()],),
-             Column(
-                children:[CardItemsPending(),CardItemsPending()],),
+              ListView(
+                children: <Widget>[
+                  CardItem(
+                    isPast: true,
+                  ),
+                  CardItem(
+                    isPast: true,
+                  ),
+                  CardItem(
+                    isPast: true,
+                  ),
+                  CardItem(
+                    isPast: true,
+                  ),
+                  CardItem(
+                    isPast: true,
+                  ),
+                  CardItem(
+                    isPast: true,
+                  ),
+                ],
+              ),
+              ListView(
+                children: [
+                  CardItem(
+                    onGoingCall: () => print('onging'),
+                  ),
+                  CardItem(
+                    onGoingCall: () => print('onging'),
+                  ),
+                  CardItem(
+                    onGoingCall: () => print('onging'),
+                  ),
+                  CardItem(
+                    onGoingCall: () => print('onging'),
+                  ),
+                  CardItem(
+                    onGoingCall: () => print('onging'),
+                  ),
+                ],
+              ),
+              ListView(
+                children: [
+                  CardItem(
+                    isPending: true,
+                    onAccpet: () => print('accept'),
+                    onDecline: () => print('decline'),
+                  ),
+                  CardItem(
+                    isPending: true,
+                    onAccpet: () => print('accept'),
+                    onDecline: () => print('decline'),
+                  ),
+                  CardItem(
+                    isPending: true,
+                    onAccpet: () => print('accept'),
+                    onDecline: () => print('decline'),
+                  ),
+                  CardItem(
+                    isPending: true,
+                    onAccpet: () => print('accept'),
+                    onDecline: () => print('decline'),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
-    ),
     );
   }
 }
 
-class CardItems extends StatelessWidget{
+class CardItem extends StatelessWidget {
+  final bool isPast;
+  final bool isPending;
+  final Function onDecline;
+  final Function onAccpet;
+  final Function onGoingCall;
+  const CardItem(
+      {Key key,
+      this.isPending = false,
+      this.onDecline,
+      this.onAccpet,
+      this.isPast = false,
+      this.onGoingCall})
+      : super(key: key);
+
   @override
-  Widget build(BuildContext context){
-    return Container(
-      padding: EdgeInsets.all(15),
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade100,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onGoingCall,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(10)),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
                   child: Image.asset(
-                    'assets/img/cartFoodSample.png',
+                    'assets/img/sampleFood.png',
+                    colorBlendMode: BlendMode.saturation,
+                    color: isPast ? Colors.black : Colors.transparent,
+                    height: 100,
+                    width: 100,
                     fit: BoxFit.cover,
                   ),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
                 ),
-              ),
-              Expanded(
-                flex: 5,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Food Name',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              'Unit price',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            Text(
+                              'LKR 3000.00',
+                              style: TextStyle(fontSize: 15),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              'Qunatity',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            Text(
+                              '3',
+                              style: TextStyle(fontSize: 15),
+                            )
+                          ],
+                        ),
+                      ]),
+                ))
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            isPending
+                ? Row(
                     children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            'Keerai Putt',
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'UnitPrice    LKR 400',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                      Text(
-                        'Quantity    2',
-                        style: TextStyle(fontSize: 10),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              child: Row(
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: onDecline,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(5)),
+                            margin: EdgeInsets.only(right: 10),
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                'Decline',
+                                style: TextStyle(color: Colors.white),
                               ),
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: onAccpet,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(5)),
+                            margin: EdgeInsets.only(left: 10),
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                'Accept',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
                     ],
+                  )
+                : SizedBox(
+                    width: 1,
                   ),
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-
-
-class CardItemsPending extends StatelessWidget{
+class CardItemsPending extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(15),
       margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -201,24 +338,20 @@ class CardItemsPending extends StatelessWidget{
                       ),
                       Row(
                         children: <Widget>[
-                        const RaisedButton(
-                            color:Colors.red,
+                          const RaisedButton(
+                            color: Colors.red,
                             textColor: Colors.white,
-                            child: Text(
-                                'Decline',
-                                style: TextStyle(fontSize: 20)
-                              ),
-                        ),
-                         SizedBox(
-                        width: 2,
-                      ),
-                        const RaisedButton(
-                          textColor: Colors.white,
-                            child: Text(
-                                'Accept',
-                                style: TextStyle(fontSize: 20)
-                              ),
-                        ),
+                            child:
+                                Text('Decline', style: TextStyle(fontSize: 20)),
+                          ),
+                          SizedBox(
+                            width: 2,
+                          ),
+                          const RaisedButton(
+                            textColor: Colors.white,
+                            child:
+                                Text('Accept', style: TextStyle(fontSize: 20)),
+                          ),
                         ],
                       ),
                       SizedBox(
